84cd69d098ce46705bd6b1b8a908dc7a3bb326c2
tuannguyen <thaituan7820@gmail.com>
2024-08-20 15:23:21 +0700

    Học useEffect và giải thích cách hoạt động useEffect

--- src/components/FeatureMovie/index.jsx
+++ src/components/FeatureMovie/index.jsx
@@ -1,37 +1,28 @@
 import PaginateIndicator from "./PaginateIndicator";
 import Movie from "./Movie";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // accesstoken: eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MjhhOTRjNWJiMWE1MjMwN2I1ZGU5OWFkYzM3NTliNyIsIm5iZiI6MTcyNDEyNDIzNi45MzMyNzksInN1YiI6IjY2YzQwYjI2ZjVlZWU1ZjdlOTc1ZjY1ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NcBSvT1OZkbJ1qEOtPBkot8dVcyL-eSaLjWm0O-fR68
 
 // Api key: 528a94c5bb1a52307b5de99adc3759b7
 
 const FeatureMovie = () => {
-  // Thay vì dùng 1 biến bình thường ta sẽ khai báo 1 state
-  const [movies, setMovies] = useState([]); // khỏi tạo cho nó 1 array rỗng
+  const [movies, setMovies] = useState([]);
 
-  // việc code như này làm diễn ra vòng lặp vô tận vì sao
-
-  /*
-    call api - thành công lấy về dữ liệu - setMovies cập nhật dữ liệu - sau đó set lại movies vào trong state - việc cập nhật này làm re-render component - được load lại - việc load lại call api lấy ra dữ liệu tiếp => vòng lặp vô tận
-  */
-
-  // đây là 1 câu lệnh bất đồng bộ
-  // fetch("https://api.themoviedb.org/3/person/popular", {
-  //   method: "GET",
-  //   headers: {
-  //     accept: "application/json",
-  //     Authorization:
-  //       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MjhhOTRjNWJiMWE1MjMwN2I1ZGU5OWFkYzM3NTliNyIsIm5iZiI6MTcyNDEyNDIzNi45MzMyNzksInN1YiI6IjY2YzQwYjI2ZjVlZWU1ZjdlOTc1ZjY1ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NcBSvT1OZkbJ1qEOtPBkot8dVcyL-eSaLjWm0O-fR68",
-  //   },
-  // }).then(async (res) => {
-  //   // console.log({ res });
-  //   const data = await res.json();
-  //   console.log({ data });
-  //   // sau khi có dữ liệu trả về vào data - tiến hành cập nhật state - tạo ra trigger tác động cho component re-render
-  //   // data là 1 object nên tham chiếu vào để lất ra results là array - tiến hành cập nhật
-  //   setMovies(data.results);
-  // });
+  // xử lý
+  useEffect(() => {
+    fetch("https://api.themoviedb.org/3/person/popular", {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MjhhOTRjNWJiMWE1MjMwN2I1ZGU5OWFkYzM3NTliNyIsIm5iZiI6MTcyNDEyNDIzNi45MzMyNzksInN1YiI6IjY2YzQwYjI2ZjVlZWU1ZjdlOTc1ZjY1ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NcBSvT1OZkbJ1qEOtPBkot8dVcyL-eSaLjWm0O-fR68",
+      },
+    }).then(async (res) => {
+      const data = await res.json();
+      setMovies(data.results);
+    });
+  }, []);
 
   console.log(movies);
 
@@ -45,65 +36,27 @@ const FeatureMovie = () => {
 export default FeatureMovie;
 
 /*
+  Xử lý vòng lặp vô tận khi gọi API - đó là ta sẽ nhét đoạn call api vào bên trong useEffect
 
-  fetch("https://api.themoviedb.org/3/person/popular", {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MjhhOTRjNWJiMWE1MjMwN2I1ZGU5OWFkYzM3NTliNyIsIm5iZiI6MTcyNDEyNDIzNi45MzMyNzksInN1YiI6IjY2YzQwYjI2ZjVlZWU1ZjdlOTc1ZjY1ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NcBSvT1OZkbJ1qEOtPBkot8dVcyL-eSaLjWm0O-fR68",
-    },
-  }).then((res) => {
-    console.log({ res });
-  });
+  tức là ta đang thông báo cho React biết rằng đoạn code call api này là 1 side Effect và nó sẽ thực hiện sau khi component này được render
 
-  Tham số đầu tiên là sẽ là api - https://api.themoviedb.org/3/person/popular
-  Tham số thứ 2 là 1 object bên trong sẽ có
-  - Phương thức "GET"
-  - headers - header là 1 object
-   + accept "Chấp nhận" application/json 
-   + Authorization: "Bearer token"
+  [] của useEffect là 1 dependencies array
 
+  Giải thích code:
 
-   .then(async (res) => {
-    // console.log({ res });
-    const data = await res.json();
-    console.log({ data });
-  });
+  Nó sẽ chạy từ trên xuống dưới
 
-  Khi mà call api thành công thì res có trả về dữ liệu có then mới bắt được - đợi res.json() để lưu data việc chờ và đợi giúp giải quyết bất đồng bộ
+  ban đầu movies sẽ được tạo ra là rỗng - kèm đó 1 hàm để setMovies
 
+  sau đó đi đến useEffects thì React sẽ lý đoạn code bên trong đó - hay chính là thàm số đầu tiên - callbacks - sau đó di chuyển tiếp xuống
 
-  Tại sao không làm như cách sau để lấy ra data
+  sau khi tới đây setMovies(data.results) tạo trigger làm component re-render chạy lại toàn bộ đoạn code ở trong component
 
-  let response = {};
-    // đây là 1 câu lệnh bất đồng bộ
-  fetch("https://api.themoviedb.org/3/person/popular", {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MjhhOTRjNWJiMWE1MjMwN2I1ZGU5OWFkYzM3NTliNyIsIm5iZiI6MTcyNDEyNDIzNi45MzMyNzksInN1YiI6IjY2YzQwYjI2ZjVlZWU1ZjdlOTc1ZjY1ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NcBSvT1OZkbJ1qEOtPBkot8dVcyL-eSaLjWm0O-fR68",
-    },
-  }).then(async (res) => {
-    // console.log({ res });
-    const data = await res.json();
-    console.log({ data });
-    response = data;
-  });
+  Và lúc này ở lần render thứ 2 thì movies đã có dữ liệu - 1 array có 20 phần tử
 
-  console.log(response)
+  chạy tiếp đến useEffect và nó sẽ kiểm tra tham số thứ 2 của useEffect thay vì tham số thứ 1 và tham số thứ 2 hiện tai là 1 dependencies array
+ và nó đang là dữ liệu rỗng. Thì nó sẽ không thực thi lại cái callbacks - hay không thực thi lại function ở tham số thứ 1 giúp tránh rơi vào vòng lập vô tận
+  
 
-  code chạy từ trên xuống dưới
-
-  B1: tạo ra response 1 object rỗng
-  B2: call api
-  B3: call thành công gán data vào response - "Bắt đầu sai ở đây"
-
-  Trong React "1 component" được re-render lại khi nào? - thứ 1 khi ta dùng state để lưu trữ dữ liệu và ta dùng 1 hàm để ta set lại State đó - cách này gọi setter function - lúc này nó mới trigger và render lại function
-
-  Cái sai ở đây là ta đang cập nhật dữ liệu - nhưng không có tạo ra 1 trigger để component re-render "không gây ra việc gọi lại function cụ thể không render lại component"
-
-
-  Để khắc phục cách này ta sử dụng Side Effect
+  Chú ý sử sụng useEFfect nên có dependencies array - vì không có thì tham số thứ 1 useEffect luôn luôn thực thi
 */
